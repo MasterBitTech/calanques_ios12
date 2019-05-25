9b0b00f6bc265923aa0682e39f46960d747c83c2
MasterBitTech <cauchy.benoit@icloud.com>
2019-05-25 16:15:55 -0400

    Initial Commit

---
+++ Les Calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Les Calanques
+//
+//  Created by Benoit Cauchy on 2019-05-25.
+//  Copyright © 2019 Benoit Cauchy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
